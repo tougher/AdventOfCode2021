cfea9e2ded4a3574e722444dc833c240805cce30
Christoffer Hoel <christoffer@hoel.dk>
2021-12-01 12:29:19 +0100

    Day 1

---
+++ Day1/Puzzle1/Program.cs
@@ -0,0 +1,13 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var increases = 0;
+
+var prevMeasurement = int.MaxValue;
+foreach (var measurement in lines.Select(int.Parse))
+{
+    if (measurement > prevMeasurement)
+        increases++;
+    prevMeasurement = measurement;
+}
+
+Console.WriteLine($"Increases: {increases}");
---
+++ Day1/Puzzle2/Program.cs
@@ -0,0 +1,16 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var increases = 0;
+var measurements = lines.Select(int.Parse).ToArray();
+
+var prevWindow = int.MaxValue;
+for (var i = 0; i < measurements.Length; i++)
+{
+    if (i > measurements.Length - 3) break;
+    var window = measurements[i] + measurements[i + 1] + measurements[i + 2];
+    if (window > prevWindow)
+        increases++;
+    prevWindow = window;
+}
+
+Console.WriteLine($"Increases: {increases}");
